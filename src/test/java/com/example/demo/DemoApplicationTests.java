0361000bfcc263a55fa5cef0b4288702e7bc6b96
Pedestriswan <pedestriswan@gmail.com>
2020-09-05 12:05:58 +0800

    Junit test

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -1,14 +1,20 @@
 package com.example.demo;
 
-import org.junit.jupiter.api.Test;
+import org.junit.Assert;
+import org.junit.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+public class DemoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	public void contextLoads() {
 		System.out.println("Test");
 	}
 
+	@Test
+	public void test() throws Exception {
+		int num = 1;
+		Assert.assertEquals(num, 1);
+	}
 }
