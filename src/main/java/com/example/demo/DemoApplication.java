6c9f9b41534cdc0d89701babcc0afc8c36750bb9
Pedestriswan <pedestriswan@gmail.com>
2020-09-06 14:23:04 +0800

    Remove useless code section

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -7,8 +7,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class DemoApplication {
 
 	public static void main(String[] args) {
-		String a = null;
-		System.out.println(a.toString());
 		SpringApplication.run(DemoApplication.class, args);
 	}
 
--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -4,6 +4,7 @@ import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 public class DemoApplicationTests {
 
